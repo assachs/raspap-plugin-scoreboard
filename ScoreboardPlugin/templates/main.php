3fb8667ef1f01af1cfaad53deb0541aaf63c17a0
andi <a.sachs@vsd.volley.de>
2025-05-23 21:25:41 +0200

    neue Raspap-Version

--- ScoreboardPlugin/templates/main.php
+++ ScoreboardPlugin/templates/main.php
@@ -29,7 +29,7 @@
             <div class="card-body">
                 <?php $status->showMessages(); ?>
                 <form role="form" action="<?php echo $__template_data['action']; ?>" method="POST" class="needs-validation" novalidate>
-                    <?php echo CSRFTokenFieldTag() ?>
+                    <?php echo \RaspAP\Tokens\CSRF::hiddenField(); ?>
                     <!-- Nav tabs -->
                     <ul class="nav nav-tabs">
                         <li class="nav-item"><a class="nav-link active" id="scoreboardsettingstab" href="#scoreboardsettings" data-bs-toggle="tab"><?php echo _("Settings"); ?></a></li>
